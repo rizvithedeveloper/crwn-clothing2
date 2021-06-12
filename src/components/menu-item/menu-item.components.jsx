fb34bf16102368cb8841ffcf03ffd4936ee5a6b6
Muhammad Rizwan <rizwan.xammin@gmail.com>
2021-06-12 19:24:45 +0500

    Broken our homepage to smaller components, finished styling our menu-item

---
+++ src/components/menu-item/menu-item.components.jsx
@@ -0,0 +1,18 @@
+import "./menu-item.styles.scss";
+
+export const MenuItem = ({ title, imageUrl, size }) => {
+  return (
+    <div className={`menu-item ${size ? size : ""}`}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      ></div>
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
